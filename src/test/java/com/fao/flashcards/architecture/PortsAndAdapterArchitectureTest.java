4715d2e305bddb4607e3b16a09d38d27b786172d
Francis Opoku <francis.opoku@gmx.de>
2025-10-19 16:23:12 +0200

    Refactor Architecture Test

--- src/test/java/com/fao/flashcards/architecture/PortsAndAdapterArchitectureTest.java
+++ src/test/java/com/fao/flashcards/architecture/PortsAndAdapterArchitectureTest.java
@@ -2,6 +2,8 @@ package com.fao.flashcards.architecture;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 
+import java.util.List;
+
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
@@ -10,20 +12,24 @@ import com.tngtech.archunit.lang.ArchRule;
 @AnalyzeClasses(packages = "com.fao.flashcards")
 public class PortsAndAdapterArchitectureTest {
 
+    private static final List<String> ALLOWED_DEPENDENCIES_SERVICE_PACKAGE = List.of(
+            "..java..",
+            "..jakarta..",
+            "..com.fao.flashcards.shared.model..",
+            "org.slf4j..",
+            "org.springframework.validation.annotation..",
+            "lombok..");
+
     @ArchTest
-    public static void servicesShouldOnlyDependOnPortsAndDomain(JavaClasses classes) {
+    public static void ocr_servicesShouldOnlyDependOnPortsAndDomain(JavaClasses classes) {
+        List<String> allowedPackages = new java.util.ArrayList<>(List.of(
+                "..ocr.application..",
+                "..ocr.model.."));
+        allowedPackages.addAll(ALLOWED_DEPENDENCIES_SERVICE_PACKAGE);
 
         ArchRule ocr = classes()
                 .that().resideInAPackage("..ocr.application.service..")
-                .should().onlyDependOnClassesThat().resideInAnyPackage(
-                        "..java..",
-                        "..jakarta..",
-                        "..ocr.application..",
-                        "..ocr.model..",
-                        "..com.fao.flashcards.shared.model..",
-                        "org.slf4j..",
-                        "org.springframework.validation.annotation..",
-                        "lombok..")
+                .should().onlyDependOnClassesThat().resideInAnyPackage(allowedPackages.toArray(new String[0]))
                 .because(
                         "Application services should only depend on application ports, domain, and standard libraries.");
 
